0a77c18db4a2cd1a727e3b72a8916bfa0d5d8777
Diego Silva <social.diegosilvatech@gmail.com>
2022-09-08 13:25:08 -0300

    feat: update stitches config

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -1,6 +1,9 @@
 import { createStitches } from '@stitches/react';
+import type * as Stitches from '@stitches/react';
 
 import { defaultTheme } from './themes';
+
+export type ConfigProps = Stitches.CSS<typeof config>;
 
 export const {
   config,
